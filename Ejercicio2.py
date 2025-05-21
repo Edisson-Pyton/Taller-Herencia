312184b9be546eabc832062d66b9e8ad2e6d19ed
Edisson-Pyton <edissonrivasherrera@gmail.com>
2025-05-20 21:10:48 -0500

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,1 +1,19 @@
-sad
+class Animal:
+    def hacer_sonido(self):
+        print("Sonido genérico")
+
+class Perro(Animal):
+    def hacer_sonido(self):
+        print("Guau")
+
+class Gato(Animal):
+    def hacer_sonido(self):
+        print("Miau")
+
+animal = Animal()
+perro = leon()
+gato = oso()
+
+animal.hacer_sonido()
+leon.hacer_sonido()
+oso.hacer_sonido()
